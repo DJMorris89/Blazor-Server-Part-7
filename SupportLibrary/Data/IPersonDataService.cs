b0afe1022d5eae3085d57297dc4c4d08724c47ce
DJMorris89 <122653895+DJMorris89@users.noreply.github.com>
2023-06-20 12:23:17 -0400

    Add files via upload

---
+++ SupportLibrary/Data/IPersonDataService.cs
@@ -0,0 +1,16 @@
+﻿using SupportLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupportLibrary.Data
+{
+    public interface IPersonDataService
+    {
+        Task CreatePerson(IPersonModel person);
+        Task DeletePerson(int id);
+        Task<List<IPersonModel>> ReadPeople();
+        Task<IPersonModel> ReadPeople(int id);
+        Task<List<IPersonModel>> SearchPeople(string searchTerm);
+        Task UpdatePerson(IPersonModel person);
+    }
+}
